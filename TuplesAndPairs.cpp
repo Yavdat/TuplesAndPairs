1bf13379eb48c0d809ec4fe25af8b5780e1c2908
Yavdat <yavdat.it@gmail.com>
2022-12-25 11:33:17 +0300

    use vector to compare dates

--- TuplesAndPairs.cpp
+++ TuplesAndPairs.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -9,15 +10,19 @@ struct Date {
 };
 
 bool operator<(const Date& lhs, const Date&rhs) {
-    if (lhs.year != rhs.year) {
-        return lhs.year < rhs.year;
-    }
-    if (lhs.month != rhs.month) {
-        return lhs.month < rhs.month;
-    }
-    return lhs.day < rhs.day;
+    // if (lhs.year != rhs.year) {
+    //     return lhs.year < rhs.year;
+    // }
+    // if (lhs.month != rhs.month) {
+    //     return lhs.month < rhs.month;
+    // }
+    // return lhs.day < rhs.day;
+    return vector<int>{lhs.year, lhs.month, lhs.day} < vector<int>{rhs.year, rhs.month, rhs.day};
 }
 
 int main() {
+    cout << (Date{2022, 6, 8} < Date{2022, 1, 26}) << endl;
+    cout << (Date{2022, 6, 8} < Date{2022, 6, 8}) << endl;
+    cout << (Date{2022, 6, 8} < Date{2022, 7, 26}) << endl;
     return 0;
 }
